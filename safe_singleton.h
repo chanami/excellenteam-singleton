0e68f1d8f9eb332f5c2518f659b5c16fb6fa3064
chanami <chanami15@gmail.com>
2018-12-23 23:24:34 -0800

    small changes

--- safe_singleton.h
+++ safe_singleton.h
@@ -10,81 +10,84 @@ class Singleton
 {
 public:
     static T* getInstance();
-    static void destroy();
 
 private:
-    Singleton(Singleton const&){};
-    Singleton& operator=(Singleton const&){};
-
-
-protected:
     static T* m_instance;
+    static pthread_mutex_t mu_lock;
 
-    pthread_mutex_t lock;
-
-    void initSynchronized();
-    void freeSynchronized();
     Singleton();
-    ~Singleton(){ freeSynchronized(); };
+    ~Singleton(){ destroy(); };
+
+    static void destroy();
+    void freeSynchronized();
+    pthread_mutex_t initSynchronized();
+
+    Singleton(Singleton const&);
+    Singleton& operator=(Singleton const&);
+
 };
 
 template<typename T>
 Singleton <T>::Singleton()
 {
-    m_instance = static_cast <T*> (this);
     initSynchronized();
+    m_instance = static_cast <T*> (this);
 }
 
 template<typename T>
-void Singleton <T>::initSynchronized()
-{
-    pthread_mutex_init(&Singleton <T>::lock, NULL);
+pthread_mutex_t Singleton<T>::mu_lock = initSynchronized();
+
+template<typename T>
+T* Singleton<T>::m_instance = NULL;
+
+template <typename T>
+pthread_mutex_t Singleton<T>::initSynchronized() {
+    if ( pthread_mutex_init ( &Singleton<T>::mu_lock, NULL ) != 0 )
+    {
+        std::cout << "mutex init failed" << std::endl;
+        freeSynchronized();
+    }
+    return mu_lock;
 }
 
 template<typename T>
 void Singleton <T>::freeSynchronized()
 {
-    pthread_mutex_destroy(&Singleton <T>::lock);
+    pthread_mutex_destroy(&Singleton <T>::mu_lock);
 }
 
-template<typename T>
-T* Singleton<T>::m_instance = 0;
 
 template<typename T>
 T* Singleton<T>::getInstance() {
 
-    pthread_mutex_lock(&Singleton <T>::lock);
+    pthread_mutex_lock(&Singleton <T>::mu_lock);
     //
-    if (m_instance == NULL)
+    if (m_instance == 0)
     {
-        pthread_mutex_lock(&Singleton <T>::lock);
-
-        if (m_instance == NULL)
+        try
         {
-            try
-            {
-                m_instance = new T;
-            }
-            catch (std::bad_alloc)
-            {
-                pthread_mutex_unlock(&Singleton <T>::lock);
-                throw ;
-            }
-            //
+            m_instance = new T;
         }
-
-        pthread_mutex_unlock(&Singleton <T>::lock);
+        catch (std::bad_alloc)
+        {
+            pthread_mutex_unlock(&Singleton <T>::mu_lock);
+            throw ;
+        }
+        //
     }
+
+    pthread_mutex_unlock(&Singleton <T>::mu_lock);
+
     return m_instance;
 }
 
 template<typename T>
 void Singleton<T>::destroy()
 {
-
     delete Singleton<T>::m_instance;
 
     Singleton<T>::m_instance = 0;
+    freeSynchronized();
 }
 
 #endif
